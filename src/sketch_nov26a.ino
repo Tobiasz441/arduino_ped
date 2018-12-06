e739f360f1986d4afd0de73f4ee88a99a9900f4b
Mateusz Tobiasz <mat.tobi@op.pl>
2018-12-06 23:36:18 +0100

    v1.1 Bug fixes

--- src/sketch_nov26a.ino
+++ src/sketch_nov26a.ino
@@ -7,46 +7,51 @@ int ldr = 13;
 int x, y;
 volatile bool wtpb = false;
 void setup() {
-  // put your setup code here, to run once:
-  attachInterrupt(digitalPinToInterrupt(3), fusslampe, RISING);
-  Serial.begin(9600);
-  g
+        // put your setup code here, to run once:
+        attachInterrupt(digitalPinToInterrupt(3), fusslampe, RISING);
+        Serial.begin(9600);
 }
 
 void loop() {
-  x = constrain(analogRead(A0), 200, 600);
-  y = map(x, 200, 600, 0, 255);
-  Serial.println(analogRead(0));
-  if (x > 350) {
-    digitalWrite(y1, LOW);
-    digitalWrite(r1, LOW);
-    digitalWrite(g1, HIGH);
-    digitalWrite(r2, HIGH);
-    digitalWrite(g2, LOW);
-  } else {
-    digitalWrite()
-  }
+        x = analogRead(A0);
+        Serial.println(x);
+        if (x > 350) {
+                digitalWrite(y1, LOW);
+                digitalWrite(r1, LOW);
+                digitalWrite(g1, HIGH);
+                digitalWrite(r2, HIGH);
+                digitalWrite(g2, LOW);
+        } else {
+                digitalWrite(r1, LOW);
+                digitalWrite(g1, LOW);
+                digitalWrite(y1, HIGH);
+                delay(500);
+                digitalWrite(y1, LOW);
+                delay(500);
+        }
 
-  if (wtpb) {
-    digitalWrite(g1, LOW);
-    digitalWrite(y1, HIGH);
-    delay(2000);
-    digitalWrite(y1, LOW);
-    digitalWrite(r1, HIGH);
-    delay(500);
-    digitalWrite(r2, LOW);
-    digitalWrite(g2, HIGH);
-    delay(10000);
-    g digitalWrite(g2, LOW);
-    digitalWrite(r2, HIGH);
-    delay(2000);
-    digitalWrite(y1, HIGH);
-    delay(2000);
-    digitalWrite(y1, LOW);
-    digitalWrite(r1, LOW);
-    digitalWrite(g1, HIGH);
-    wtpb = false;
-  }
+        if (wtpb) {
+                digitalWrite(g1, LOW);
+                digitalWrite(y1, HIGH);
+                delay(2000);
+                digitalWrite(y1, LOW);
+                digitalWrite(r1, HIGH);
+                delay(500);
+                digitalWrite(r2, LOW);
+                digitalWrite(g2, HIGH);
+                delay(10000);
+                digitalWrite(g2, LOW);
+                digitalWrite(r2, HIGH);
+                delay(2000);
+                digitalWrite(y1, HIGH);
+                delay(2000);
+                digitalWrite(y1, LOW);
+                digitalWrite(r1, LOW);
+                digitalWrite(g1, HIGH);
+                wtpb = false;
+        }
 }
 
-void fusslampe() { wtpb = true; }
+void fusslampe() {
+        wtpb = true;
+}
