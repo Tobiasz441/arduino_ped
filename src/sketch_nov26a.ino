0511f078b633d80a8eafad88b014c328c00d82d7
Mateusz Tobiasz <mat.tobi@op.pl>
2018-12-09 21:34:31 +0100

    v1.3 Added functions

--- src/sketch_nov26a.ino
+++ src/sketch_nov26a.ino
@@ -1,3 +1,4 @@
+// Initialisierung von Variablen
 int r1 = 22;
 int g1 = 24;
 int y1 = 26;
@@ -6,52 +7,73 @@ int g2 = 30;
 int ldr = 13;
 int x, y;
 volatile bool wtpb = false;
+// Initialisierung der Funktionen
+void fussampel() {
+        // Phase 2
+        digitalWrite(g1, LOW);
+        digitalWrite(y1, HIGH);
+        delay(2000);
+        // Phase 3
+        digitalWrite(y1, LOW);
+        digitalWrite(r1, HIGH);
+        delay(500);
+        // Fussgängerampel an
+        digitalWrite(r2, LOW);
+        digitalWrite(g2, HIGH);
+        delay(10000);
+        // Fussgängerampel aus
+        digitalWrite(g2, LOW);
+        digitalWrite(r2, HIGH);
+        delay(2000);
+        // Phase 2
+        digitalWrite(y1, HIGH);
+        delay(2000);
+        // Am ende wird wtpb auf falsch gesetzt um ein loop zu vermeiden
+        wtpb = false;
+}
+void tag() {
+        // Wenn hell funktioniert die Ampel wie sonst
+        digitalWrite(y1, LOW);
+        digitalWrite(r1, LOW);
+        digitalWrite(g1, HIGH);
+        digitalWrite(r2, HIGH);
+        digitalWrite(g2, LOW);
+}
+void nacht() {
+        // Wenn dunkel blinkt nur das gelbe licht
+        digitalWrite(y1, LOW);
+        delay(500);
+        digitalWrite(y1, HIGH);
+        delay(500);
+        digitalWrite(r1, LOW);
+        digitalWrite(g1, LOW);
+        digitalWrite(r1, LOW);
+        digitalWrite(g1, LOW);
+        digitalWrite(r2, LOW);
+        digitalWrite(g2, LOW);
+}
+
 void setup() {
-        // put your setup code here, to run once:
+        // Initialisierung des Interrupts und Serial Monitors
         attachInterrupt(digitalPinToInterrupt(3), fusslampe, RISING);
         Serial.begin(9600);
 }
 
 void loop() {
-        x = analogRead(A0);
-        Serial.println(x);
-        if (x > 350) {
-                digitalWrite(y1, LOW);
-                digitalWrite(r1, LOW);
-                digitalWrite(g1, HIGH);
-                digitalWrite(r2, HIGH);
-                digitalWrite(g2, LOW);
-        } else {
-                digitalWrite(r1, LOW);
-                digitalWrite(g1, LOW);
-                digitalWrite(y1, HIGH);
-                delay(500);
-                digitalWrite(y1, LOW);
-                delay(500);
-        }
+        x = constrain(analogRead(A0), 200, 600);
+        Serial.println(analogRead(0));
 
+        if (x > 250) {
+                tag();
+        } else {
+                nacht();
+        }
+        // Wenn wtpb = richtig wird dieser Code ausgeführt
         if (wtpb) {
-                digitalWrite(g1, LOW);
-                digitalWrite(y1, HIGH);
-                delay(2000);
-                digitalWrite(y1, LOW);
-                digitalWrite(r1, HIGH);
-                delay(500);
-                digitalWrite(r2, LOW);
-                digitalWrite(g2, HIGH);
-                delay(10000);
-                digitalWrite(g2, LOW);
-                digitalWrite(r2, HIGH);
-                delay(2000);
-                digitalWrite(y1, HIGH);
-                delay(2000);
-                digitalWrite(y1, LOW);
-                digitalWrite(r1, LOW);
-                digitalWrite(g1, HIGH);
-                wtpb = false;
+                fussampel();
         }
 }
-
+// Interrupt, wenn Button gedrückt setze wtpb auf true
 void fusslampe() {
         wtpb = true;
 }
